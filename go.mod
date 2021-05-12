61c5cb937377f952d7ccbfc0a51386e3a9e25660
Mher Melkonyan <78207603+melkonyanup@users.noreply.github.com>
2021-05-12 20:58:38 +0400

    Update

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/hongjinui/go-blockchain2
+
+go 1.15
+
+require (
+	github.com/boltdb/bolt v1.3.1
+	golang.org/x/crypto v0.0.0-20210506145944-38f3c27a63bf
+)
---
+++ proofofwork.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
+	"math"
+	"math/big"
+)
+
+const targetBits = 15
+
+var (
+	target   big.Int
+	maxNonce = math.MaxInt64
+)
+
+// ProofOfWork represents a proof-of-work
+type ProofOfWork struct {
+	block  *Block
+	target *big.Int
+}
+
+// NewProofOfWork builds and returns a ProofOfWork
+func NewProofOfWork(b *Block) *ProofOfWork {
+
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+
+	pow := &ProofOfWork{b, target}
+
+	return pow
+
+}
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.HashTransactions(),
+			intToHex(pow.block.Timestamp),
+			intToHex(int64(targetBits)),
+			intToHex(int64(nonce)),
+		},
+		[]byte{},
+	)
+	return data
+}
+
+// Run performs a proof-of-work
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+
+	fmt.Printf("Mining a new block")
+
+	for nonce <= maxNonce {
+		data := pow.prepareData(nonce)
+
+		hash = sha256.Sum256(data)
+		fmt.Printf("\r%x", hash)
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.target) == -1 {
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Printf("\n\n")
+
+	return nonce, hash[:]
+}
+
+// Vaildate validates block's Pow
+func (pow *ProofOfWork) Vaildate() bool {
+
+	var hashInt big.Int
+
+	data := pow.prepareData(pow.block.Nonce)
+	hash := sha256.Sum256(data)
+	hashInt.SetBytes(hash[:])
+
+	isVaild := hashInt.Cmp(pow.target) == -1
+
+	return isVaild
+}
